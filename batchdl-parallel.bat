515ee98bbded400dd565320214a110e299d82473
Gregorius Bryan <dev@selubi.tech>
2023-05-21 20:06:50 +0900

    Adds autoupdate

--- batchdl-parallel.bat
+++ batchdl-parallel.bat
@@ -1,1 +1,2 @@
+.\bin\yt-dlp.exe -U./bin/yt-dlp.exe -U
 for /F "eol=# tokens=*" %%A in (dl-list.txt) do start .\bin\yt-dlp.exe --config-location .\yt-dlp-config.txt %%A
--- batchdl-sequential.bat
+++ batchdl-sequential.bat
@@ -1,1 +1,2 @@
+.\bin\yt-dlp.exe -U
 for /F "eol=# tokens=*" %%A in (dl-list.txt) do .\bin\yt-dlp.exe --config-location .\yt-dlp-config.txt %%A
